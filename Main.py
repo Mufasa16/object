9fb737927dacc7fa1b2f020db521bfb0ac56f30d
Mufasa16 <81098853+Mufasa16@users.noreply.github.com>
2022-06-16 16:53:56 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,38 @@
-class student:
-
-  def __init__(mysillyobject, name, age, score):
+from ast import Str
 
 
-    mysillyobject.name = string
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self,name,age,tracks,score):
+        self.name =str(name)
+        self.age =int(age)
+        self.tracks =list(tracks)
+        self.score =float(score)
 
-    mysillyobject.age = integer 
-    mysillyobject.score =integer
+    def change_name(self, change_name):
+        self.change_name= change_name
+        print("The student new name is", change_name)
 
-  def myfunc(abc):
+    def change_age(self, change_age):
+        self.change_age= change_age
+        print("The student new age is", change_age) 
 
-    print("score " )
+    def add_track(self,add_track):
+        self.add_track= add_track
+        print("The student new track is ", add_track)
 
-p1 = student(, 36)
+    def get_score(self):
+        return("The new student age is ",self.score)
 
-p1.myfunc()
+
+
+
+
+
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+
+# Expected methods
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score()
